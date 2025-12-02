07347df0982a934559915d15e1066c17e0833c6a
Himanshu Kumar Singh <R10651@apexuniversity.co.in>
2025-12-02 21:44:32 +0530

    BinarySearch

---
+++ DSA_with_JAVA/00_PATTERNS/03_Binary_Search/BookAllocation.java
@@ -0,0 +1,49 @@
+public class BookAllocation{
+    public static boolean isPossible(int arr[] , int n , int mid){
+        int students = 1;
+        int currSum = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>mid){
+                return false;
+            }
+            if(currSum+arr[i]>mid){
+                students++;
+                currSum = arr[i];
+                if(students>n) return false;
+            }else{
+                currSum+=arr[i];
+            }
+        }
+        return true;
+    }
+    public static int findPages(int arr[] , int n){
+        //max = 10   ;   min = 0
+        int max = 0;
+        if(arr.length<n) return -1;
+        int result = 0;
+        for(int i=0;i<arr.length;i++){
+            max+=arr[i];
+        }
+        int si = 0;
+        int ei = max;
+        while(si<=ei){
+            int mid = si+(ei-si)/2;
+            if(isPossible(arr , n , mid)){
+                result = mid;
+                ei = mid-1;
+            }else{
+                si = mid+1;
+            }
+            
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // int pages[] = {2,1,3,4};
+        int pages[] = { 10,20,30,40};
+        int m = 2; //Number of students
+        int ans = findPages(pages , m );
+        System.out.println("Minimum Pages  : "+ans);
+    }
+}
